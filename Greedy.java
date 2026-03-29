c89a8ba7741cf28a1cde54a432e25d3ecc6471b9
kirti tiwari <kirtitiwari0312@gmail.com>
2026-03-29 12:10:30 +0530

    new code added

--- Greedy.java
+++ Greedy.java
@@ -32,10 +32,63 @@ public class Greedy{
                 v+=value[idx];
             }else{
                 v+=ratio[i][1]*capacity;
+                capacity=0;
+                break;
             }
         }
         return v;
     }
+    public static int minAbsoluteDiff(int a[],int b[]){
+        Arrays.sort(a);
+        Arrays.sort(b);
+        int sum=0;
+        for(int i=0;i<a.length;i++){
+            if(a[i]>b[i]){
+                sum+=(a[i]-b[i]);
+            }else{
+                sum+=(b[i]-a[i]);
+            }
+        }
+        return sum;
+    }
+    public static int maxLengthChainPair(int pairs[][]){
+        Arrays.sort(pairs,Comparator.comparingDouble(o->o[1]));
+        int chainLength=1;
+        int pairEnd=pairs[0][1];
+        for(int i=1;i<pairs.length;i++){
+            if(pairs[i][0]>pairEnd){
+                chainLength++;
+                pairEnd=pairs[i][1];
+            }
+        }
+        return chainLength;
+    }
+    public static int minCoin(  Integer coins[],int amount){
+        Arrays.sort(coins,Collections.reverseOrder());   //note-cxant be called for this primitive datatype (collectrions)
+        int count=0;
+        ArrayList<Integer> a=new ArrayList<>();
+        for(int i=0;i<coins.length;i++){
+            if(coins[i]<=amount){
+               while(coins[i]<=amount){
+                count++;
+                amount-=coins[i];
+                a.add(coins[i]);
+            }}
+        }
+        System.out.println(a);
+        return count;
+    }
+    static class Job{
+        int deadline;
+        int profit;
+        int id;
+        Job(int i,int d,int p){
+            deadline=d;
+            profit=p;
+            id=i;
+        }
+    }
+
     public static void main(String[]args){
         int start[]={1,3,0,5,8,5};
         int end[]={2,4,6,7,9,9};
@@ -43,5 +96,33 @@ public class Greedy{
         int value[]={60,100,120};
         int weight[]={10,20,30};
         System.out.println(knapsack(value,weight,50));
+        int a[]={4,1,8,7};
+        int b[]={2,3,6,5};
+       System.out.println(minAbsoluteDiff(a, b));
+       int pairs[][]={{5,24},{39,60},{5,28},{27,40},{50,90}};
+       System.out.println(maxLengthChainPair(pairs));
+       Integer coins[]={1,5,10,20,50,100,500,2000};
+       System.out.println(minCoin(coins,590));
+       int jobsInfo[][]={{4,20},{1,10},{1,40},{1,30}};
+
+       ArrayList<Job> jobs=new ArrayList<>();
+       for(int i=0;i<jobsInfo.length;i++){
+        jobs.add(new Job(i,jobsInfo[i][0],jobsInfo[i][1]));
+       }
+       Collections.sort(jobs,(obj1,obj2)->obj2.profit-obj1.profit);
+       ArrayList<Integer> seq=new ArrayList<>();
+       int time=0;
+       for(int i=0;i<jobs.size()-1;i++){
+            Job curr=jobs.get(i);
+            if(curr.deadline>time){
+                seq.add(curr.id);
+                time++;
+            }
+       }
+       System.out.println("max jobs="+seq.size());
+       for(int i=0;i<seq.size();i++){
+        System.out.print(seq.get(i)+" ");
+       }
+
     }
 }
